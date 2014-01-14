ce3f2a0e577a83c674cce9f03b04dcb5edc50603
Kenneth Kan <ken@pixbi.com>
2014-01-13 22:47:59 -0500

    Make component out of Riot

---
+++ component.js
@@ -0,0 +1,16 @@
+{
+  "name": "riotjs",
+  "repo": "kenhkan/riotjs",
+  "description": "Riot component",
+  "version": "0.9.8",
+  "keywords": [
+    "MVP",
+    "MVC",
+    "framework",
+    "library"
+  ],
+  "dependencies": {
+  },
+  "scripts": ["riot.js"],
+  "license": "MIT"
+}
